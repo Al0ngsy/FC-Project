e4294569293d6fd321fce4fd1af1ab871575eae8
5aint2ero <33838537+5aint2ero@users.noreply.github.com>
2023-06-15 20:11:08 +0200

    test faas

---
+++ cloud_faas/ts-built/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+// src/index.ts
+const hello = (req, res) => res.send("Hello!");
+exports.hello = hello;
