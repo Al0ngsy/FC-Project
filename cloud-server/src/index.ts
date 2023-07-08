9065d99dfd38e9fac750f5cab15d2ed6f8904d64
Le Quoc Anh Tran <lequocanhtr@gmail.com>
2023-07-08 20:30:23 +0200

    add log

--- cloud-server/src/index.ts
+++ cloud-server/src/index.ts
@@ -8,6 +8,7 @@ import { chaosAfterMonkey, chaosMonkey } from "./chaosMonkey";
 import { config } from "./config";
 import { dbAdd } from "./db/database";
 import { processSensorData } from "./task/processSensorData";
+import { log } from "./utils";
 
 const app = new Koa();
 const router = new KoaRouter();
@@ -20,6 +21,9 @@ router.post(
 		const data = ctx.request.body as StoredData;
 		dbAdd(data);
 		const result = processSensorData(data);
+		log(
+			`Result from processing data with id ${data.dataUniqueId} is ${result}. Return result.`
+		);
 		ctx.status = 200;
 		ctx.body = {
 			result,
--- cloud-server/src/task/processSensorData.ts
+++ cloud-server/src/task/processSensorData.ts
@@ -1,12 +1,12 @@
 import { StoredData } from "../@types/data";
+import { log } from "../utils";
 
 export const processSensorData = (data: StoredData) => {
 	let totalSum = 0;
-
+	log(`Process data with id ${data.dataUniqueId}`);
 	for (let i = 0; i < data.data.length; i++) {
 		let charCode = (data.data as string).charCodeAt(i);
 		isNaN(charCode) === false ? (totalSum += charCode) : null;
 	}
-
 	return totalSum;
 };
