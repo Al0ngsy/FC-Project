8916248b1e69ae1a8d9bb34d112e995d1712690a
5aint2ero <33838537+5aint2ero@users.noreply.github.com>
2023-07-08 13:16:11 +0200

    added terraform file

---
+++ cloud_setup/main.tf
@@ -0,0 +1,53 @@
+provider "google" {
+  credentials = file("<credentials_file_path>")
+  project     = "<project_id>"
+  region      = "us-central1"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "<vm_name>"
+  machine_type = "<machine_type>"
+  zone         = "<zone>"
+
+  boot_disk {
+    initialize_params {
+      image = "projects/ubuntu-os-cloud/global/images/family/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_compute_firewall" "http" {
+  name    = "default-allow-http"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "https" {
+  name    = "default-allow-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
--- edge-server/src/config.ts
+++ edge-server/src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
 	NODE_ENV: process.env.NODE_ENV || "dev",
-	SERVER_URL: process.env.SERVER_URL || "http://localhost",
-	SERVER_PORT: process.env.PORT || 5559,
+	SERVER_URL: process.env.SERVER_URL || "http://23.251.137.133",
+	SERVER_PORT: process.env.PORT || 80, //5559,
 	SERVER_DATA_RECEIVER_ENDPOINT_API:
 		process.env.SERVER_DATA_RECEIVER_ENDPOINT_API || "/save/data",
 	BACKOFF_TIME_MS: process.env.BACKOFF_TIME || 10000,
